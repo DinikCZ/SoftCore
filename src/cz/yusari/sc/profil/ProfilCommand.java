a196563cb61b2894301e34a08b13b2ec04a42a92
DinikCZ <Monster79921@seznam.cz>
2018-01-21 14:12:35 +0100

    .

---
+++ src/cz/yusari/sc/profil/ProfilCommand.java
@@ -0,0 +1,14 @@
+package cz.yusari.sc.profil;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ProfilCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String arg, String[] args) {
+        return false;
+    }
+
+}
